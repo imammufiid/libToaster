79d0242301ea147af81a59ab9a7006b0337f0460
imufiid <imufiid98@gmail.com>
2020-11-23 14:32:33 +0700

    add margin layout toast_layout.xml

--- lib/src/main/java/com/mufiid/lib/Toaster.java
+++ lib/src/main/java/com/mufiid/lib/Toaster.java
@@ -83,7 +83,7 @@ public class Toaster {
         }
 
         toastMessage.setText(message);
-        currentToast.setGravity(Gravity.TOP|Gravity.RIGHT, 0, 80);
+        currentToast.setGravity(Gravity.TOP|Gravity.RIGHT, 0, 0);
         currentToast.setView(toastLayout);
 
         if (!allowQueue){
